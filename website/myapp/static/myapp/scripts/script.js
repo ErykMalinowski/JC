ba636b903fe35dbb39571b9e2d8ecb319900e182
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-02-16 21:58:13 +0100

    add hamburger menu to the website

--- website/myapp/static/myapp/scripts/script.js
+++ website/myapp/static/myapp/scripts/script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+// show/hide page navigation
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.page-navigation');
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('page-navigation--visible');
+}
+
+hamburger.addEventListener('click', handleClick);
